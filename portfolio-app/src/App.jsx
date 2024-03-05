28ceb0fc962ac341a7b31b3b0b0193340c6f1913
TamilselvanChandrakumar <tamilbin001@gmail.com>
2024-03-05 23:12:51 +0530

    first commit

---
+++ portfolio-app/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { Home } from "./component/home/Home";
+
+function App() {
+  return (
+    <>
+      <Home></Home>
+    </>
+  );
+}
+
+export default App;
---
+++ portfolio-app/src/component/home/Home.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+import "../home/Home.css";
+export const Home = () => {
+  return <div>Home</div>;
+};
