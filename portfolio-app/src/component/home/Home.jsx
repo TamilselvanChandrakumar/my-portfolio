ce23ad73422fae655fc52e58296c5d425e5be384
TamilselvanChandrakumar <tamilbin001@gmail.com>
2024-03-06 23:05:58 +0530

    first commit

--- portfolio-app/src/component/home/Home.jsx
+++ portfolio-app/src/component/home/Home.jsx
@@ -4,7 +4,16 @@ export const Home = () => {
   return (
     <>
       <section id="home">
-        <h1>home</h1>
+        <div className="home-container">
+          <div className="home-col">
+            <h3>Hello,my name is </h3>
+            <h1>Tamilselvan C</h1>
+            <h2>
+              And I'm a <span> Web Developer</span>
+            </h2>
+            <button className="btn-wrapper">Hire Me</button>
+          </div>
+        </div>
       </section>
     </>
   );
