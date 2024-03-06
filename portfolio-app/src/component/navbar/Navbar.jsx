227c95e451e5741773e69de8c002b08fadb476d5
TamilselvanChandrakumar <tamilbin001@gmail.com>
2024-03-07 00:51:41 +0530

    first commit

--- portfolio-app/src/component/navbar/Navbar.jsx
+++ portfolio-app/src/component/navbar/Navbar.jsx
@@ -9,11 +9,23 @@ const Navbar = () => {
         </div>
         <nav className="navbar-links-container">
           <ul className="navbar-links">
-            <li>Home</li>
-            <li>About</li>
-            <li>Skills</li>
-            <li>Projects</li>
-            <li>Contact</li>
+            <li>
+              <a href="#home">Home</a>
+            </li>
+            <li>
+              <a href="#about">About</a>
+            </li>
+            <li>
+              {" "}
+              <a href="#skills">Skills</a>
+            </li>
+            <li>
+              {" "}
+              <a href="#projects">Projects</a>
+            </li>
+            <li>
+              <a href="#contact">Contact</a>
+            </li>
           </ul>
         </nav>
       </div>
