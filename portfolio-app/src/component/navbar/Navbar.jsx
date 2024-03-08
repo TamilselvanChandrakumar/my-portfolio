c5f6b996cd0f43d1d923e1f511f01be17a53f74d
TamilselvanChandrakumar <tamilbin001@gmail.com>
2024-03-08 23:49:54 +0530

    first commit

--- portfolio-app/src/component/navbar/Navbar.jsx
+++ portfolio-app/src/component/navbar/Navbar.jsx
@@ -1,6 +1,12 @@
 import React, { useState } from "react";
 import "../navbar/Navbar.css";
 import { FaBars } from "react-icons/fa6";
+import { FaDharmachakra } from "react-icons/fa6";
+import { FaHouseUser } from "react-icons/fa6";
+import { FaRegFolderOpen } from "react-icons/fa6";
+import { FaPhone } from "react-icons/fa6";
+import { FaGraduationCap } from "react-icons/fa6";
+import { FaCircleUser } from "react-icons/fa6";
 const Navbar = () => {
   const [sidenav, setSideNav] = useState(false);
   const handleSidenav = () => {
@@ -12,7 +18,8 @@ const Navbar = () => {
       <div className="navbar-container">
         <div className="navbar-text">
           <h1>
-            Myfortpo<span>lio</span>
+            <FaDharmachakra color="black" size={30}></FaDharmachakra> Myfortpo
+            <span>lio</span>
           </h1>
         </div>
         <div className="hamburger" onClick={handleSidenav}>
@@ -21,20 +28,23 @@ const Navbar = () => {
         <nav className={`navbar-links-container ${sidenav ? "active" : null}`}>
           <ul className="navbar-links">
             <li>
-              <a href="#home">Home</a>
+              <FaHouseUser></FaHouseUser> <a href="#home">Home</a>
             </li>
             <li>
+              <FaCircleUser></FaCircleUser>
               <a href="#about">About</a>
             </li>
             <li>
               {" "}
-              <a href="#skills">Skills</a>
+              <FaGraduationCap></FaGraduationCap> <a href="#skills">Skills</a>
             </li>
             <li>
               {" "}
+              <FaRegFolderOpen></FaRegFolderOpen>
               <a href="#projects">Projects</a>
             </li>
             <li>
+              <FaPhone></FaPhone>
               <a href="#contact">Contact</a>
             </li>
           </ul>
