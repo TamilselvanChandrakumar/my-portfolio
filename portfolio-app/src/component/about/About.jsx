7c45518f7818d661b9bfddf6762fb82d58706dea
TamilselvanChandrakumar <tamilbin001@gmail.com>
2024-03-11 23:54:57 +0530

    first commit

--- portfolio-app/src/component/about/About.jsx
+++ portfolio-app/src/component/about/About.jsx
@@ -10,12 +10,13 @@ const About = () => {
               I'm Tamilselvan and I'm a <span>Webdeveloper</span>{" "}
             </h2>
             <p className="detail">
-              I'm a fresher.I finished my BE-CSE in Shree Venkateshwara Hi-Tech
-              Engineering college-Erode.I completed my internship (MERN stack
-              course) in Kitkat Software Technologies at Coimbatore..And I am a
-              person who exploring new Technologies..I have leadership qualities
-              and work with the team efficiently.I'm a Hard working person and
-              ability to work under pressure..
+              I have completed my ME-BCS in Rathinam Technical
+              Campus,Coimbatore. I completed course (MERN stack course) in
+              Kitkat Software Technologies at Coimbatore..And I am a person who
+              exploring new Technologies..I have leadership qualities and work
+              with the team efficiently.Looking to start the career as an
+              entry-level web developer with a reputed firm driven by
+              technology.
             </p>
             <button>Download CV</button>
           </div>
