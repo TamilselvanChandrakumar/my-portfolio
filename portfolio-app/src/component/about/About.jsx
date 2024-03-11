26e21e2b00448f84f569f0c50f50259094af4b7c
TamilselvanChandrakumar <tamilbin001@gmail.com>
2024-03-11 23:24:54 +0530

    first commit

--- portfolio-app/src/component/about/About.jsx
+++ portfolio-app/src/component/about/About.jsx
@@ -5,7 +5,23 @@ const About = () => {
     <>
       <section id="about">
         <div className="about-container">
-          <h1 className="about-text">About me</h1>
+          <div className="aboutcontent">
+            <h2 className="title">
+              I'm Tamilselvan and I'm a{" "}
+              <span className="webdev">Webdeveloper</span>{" "}
+            </h2>
+            <br></br>
+            <p className="detail">
+              I'm a fresher.I finished my BE-CSE in Shree Venkateshwara Hi-Tech
+              Engineering college-Erode.I completed my internship (MERN stack
+              course) in Kitkat Software Technologies at Coimbatore..And I am a
+              person who exploring new Technologies..I have leadership qualities
+              and work with the team efficiently.I'm a Hard working person and
+              ability to work under pressure..
+            </p>
+            <br></br>
+            <button className="cvbtn">Download CV</button>
+          </div>
         </div>
       </section>
     </>
